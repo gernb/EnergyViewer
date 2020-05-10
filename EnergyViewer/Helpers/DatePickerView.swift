4846d7710d8cfed2c40df315dae86b4cb3cdbce0
gernb <gern@1dot0.net>
2020-05-10 11:58:05 -0700

    Add month/year picker to the date picker

--- EnergyViewer/Helpers/DatePickerView.swift
+++ EnergyViewer/Helpers/DatePickerView.swift
@@ -25,41 +25,25 @@ struct DatePickerView: View {
     }
 
     @ObservedObject private var vm: ViewModel
-    @Environment(\.calendar) private var calendar
-
-    private static var monthAndYear: DateFormatter {
-        let formatter = DateFormatter()
-        formatter.dateFormat = "MMMM yyyy"
-        return formatter
-    }
 
     var body: some View {
-        VStack {
-            HStack {
-                Image(systemName: "arrowshape.turn.up.left.circle.fill")
-                    .frame(minWidth: 40, minHeight: 40)
-                    .onTapGesture {
-                        self.vm.currentMonth = self.calendar.date(byAdding: .month, value: -1, to: self.vm.currentMonth)!
-                    }
-                Spacer()
-                Text(Self.monthAndYear.string(from: vm.currentMonth))
-                Spacer()
-                Image(systemName: "calendar.circle.fill")
-                    .foregroundColor(vm.todayColour)
-                    .frame(minWidth: 40, minHeight: 40)
-                    .onTapGesture {
-                        self.vm.currentMonth = Date()
-                    }
-                Image(systemName: "arrowshape.turn.up.right.circle.fill")
-                    .frame(minWidth: 40, minHeight: 40)
-                    .onTapGesture {
-                        self.vm.currentMonth = self.calendar.date(byAdding: .month, value: 1, to: self.vm.currentMonth)!
-                    }
+        ZStack {
+            CalendarView()
+                .transition(.move(edge: .trailing))
+                .isHidden(vm.showMonthAndYearPicker)
+
+            MonthAndYearPickerView() { date in
+                if let date = date {
+                    self.vm.currentMonth = date
+                }
+                withAnimation {
+                    self.vm.showMonthAndYearPicker = false
+                }
             }
-            MonthView(month: vm.currentMonth)
-                .environmentObject(vm)
+            .transition(.move(edge: .trailing))
+            .isHidden(!vm.showMonthAndYearPicker)
         }
-        .scaledToFit()
+        .environmentObject(vm)
         .padding()
     }
 }
@@ -67,6 +51,7 @@ struct DatePickerView: View {
 fileprivate final class ViewModel: ObservableObject {
     @Published private(set) var selectedDate: Date
     @Published var currentMonth: Date
+    @Published var showMonthAndYearPicker: Bool
     let backgroundColour: Color
     let foregroundColour: Color
     let todayColour: Color
@@ -82,6 +67,7 @@ fileprivate final class ViewModel: ObservableObject {
 
         self.selectedDate = selectedDate
         self.currentMonth = selectedDate
+        self.showMonthAndYearPicker = false
         self.backgroundColour = backgroundColour
         self.foregroundColour = foregroundColour
         self.todayColour = todayColour
@@ -92,6 +78,117 @@ fileprivate final class ViewModel: ObservableObject {
     func select(date: Date) {
         selectedDate = date
         onDateSelected?(date)
+    }
+}
+
+fileprivate extension View {
+    func isHidden(_ hidden: Bool) -> some View {
+        modifier(HiddenModifier(hidden: hidden))
+    }
+}
+
+fileprivate struct HiddenModifier: ViewModifier {
+    let hidden: Bool
+
+    func body(content: Content) -> some View {
+        Group {
+            if hidden {
+                content.hidden()
+            } else {
+                content
+            }
+        }
+    }
+}
+
+fileprivate struct CalendarView: View {
+    @Environment(\.calendar) private var calendar
+    @EnvironmentObject private var vm: ViewModel
+
+    private static let monthAndYear: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "MMMM yyyy"
+        return formatter
+    }()
+
+    var body: some View {
+        VStack {
+            HStack {
+                Image(systemName: "arrowshape.turn.up.left.circle.fill")
+                    .frame(minWidth: 40, minHeight: 40)
+                    .onTapGesture {
+                        self.vm.currentMonth = self.calendar.date(byAdding: .month, value: -1, to: self.vm.currentMonth)!
+                    }
+                Spacer()
+                Text(Self.monthAndYear.string(from: vm.currentMonth))
+                    .onTapGesture { withAnimation { self.vm.showMonthAndYearPicker = true } }
+                Spacer()
+                Image(systemName: "calendar.circle.fill")
+                    .foregroundColor(vm.todayColour)
+                    .frame(minWidth: 40, minHeight: 40)
+                    .onTapGesture {
+                        self.vm.currentMonth = Date()
+                    }
+                Image(systemName: "arrowshape.turn.up.right.circle.fill")
+                    .frame(minWidth: 40, minHeight: 40)
+                    .onTapGesture {
+                        self.vm.currentMonth = self.calendar.date(byAdding: .month, value: 1, to: self.vm.currentMonth)!
+                    }
+            }
+            MonthView(month: vm.currentMonth)
+        }
+        .scaledToFit()
+    }
+}
+
+fileprivate struct MonthAndYearPickerView: View {
+    let onDismiss: (Date?) -> Void
+    @Environment(\.calendar) private var calendar
+    @EnvironmentObject private var vm: ViewModel
+    @State private var selectedMonth: Int = 1
+    @State private var selectedYear: Int = 2020
+
+    private var years: [Int] {
+        let year = calendar.component(.year, from: Date())
+        guard year >= 2015 else { return [year] }
+        return Array(2015 ... year)
+    }
+
+    var body: some View {
+        VStack {
+            HStack {
+                Button("Cancel") {
+                    self.onDismiss(nil)
+                }
+                Spacer()
+                Button("Done") {
+                    let components = DateComponents(year: self.selectedYear, month: self.selectedMonth + 1, day: 1)
+                    let date = self.calendar.date(from: components)
+                    self.onDismiss(date)
+                }
+            }
+            GeometryReader { geometry in
+                HStack {
+                    Picker(selection: self.$selectedMonth, label: EmptyView()) {
+                        ForEach(self.calendar.monthSymbols.indices) { month in
+                            Text(self.calendar.monthSymbols[month])
+                        }
+                    }
+                    .frame(width: geometry.size.width / 2)
+                    Picker(selection: self.$selectedYear, label: EmptyView()) {
+                        ForEach(self.years, id: \.self) { year in
+                            Text("\(year)".replacingOccurrences(of: ",", with: ""))
+                        }
+                    }
+                    .frame(width: geometry.size.width / 2)
+                }
+            }
+            Spacer()
+        }
+        .onAppear() {
+            self.selectedMonth = self.calendar.component(.month, from: self.vm.currentMonth) - 1
+            self.selectedYear = self.calendar.component(.year, from: self.vm.currentMonth)
+        }
     }
 }
 
